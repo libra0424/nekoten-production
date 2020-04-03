8e6a41441d15c4d99e15448ad830f1f6515fdbfc
Tanaka Ryo <yuzuku@TanakanoMacBook-Pro.local>
2020-04-03 16:32:16 +0900

    rspecの初期設定とuserバリデーションのテスト追加

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  before do
+    @user = build(:user)
+  end
+
+  describe 'バリデーション' do
+    it 'テストデータがバリデーションを通過すること' do
+      expect(@user).to be_valid
+    end
+
+    it '名前が空だと登録できない' do
+      @user.name = ""
+      expect(@user).not_to be_valid
+    end
+
+    it '名前が50文字を超えると登録できない' do
+      @user.name = "a" * 51
+      expect(@user).not_to be_valid
+    end
+  end
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -60,4 +60,8 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  RSpec.configure do |config|
+    config.include Devise::Test::IntegrationHelpers, type: :request #sign_inヘルパーを提供してくれます
+    config.include FactoryBot::Syntax::Methods #ついでにFactoryBotもincludeしておきます
+  end
 end
