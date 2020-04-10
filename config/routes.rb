a26e6f63ea42522aebefce15c7ec5f2b4693d4da
Tanaka Ryo <yuzuku@TanakanoMacBook-Pro.local>
2020-04-10 23:56:42 +0900

    いいね機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
   get '/pages/home', to: 'pages#home'
   resources :posts, only: %i(new create index show destroy) do
     resources :photos, only: %i(create)
+    resources :likes, only: %i(create destroy)
   end
 end
