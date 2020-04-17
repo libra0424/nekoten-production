30108414b98ab88ae9fda3e4dfec38757b192cef
Tanaka Ryo <yuzuku@TanakanoMacBook-Pro.local>
2020-04-17 10:51:11 +0900

    １対多の間違えたカラムを削除

---
+++ db/migrate/20200417014133_remove_cat_from_post.rb
@@ -0,0 +1,6 @@
+class RemoveCatFromPost < ActiveRecord::Migration[5.2]
+  def change
+    remove_foreign_key :posts, :cat
+    remove_column :posts, :cat
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_13_073537) do
+ActiveRecord::Schema.define(version: 2020_04_17_014133) do
 
   create_table "cats", force: :cascade do |t|
     t.string "name"
