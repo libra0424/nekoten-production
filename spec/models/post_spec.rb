f89640618df32e40dffea6d5f119de854c9eb1e9
yuzuku <yuzuku04@gmail.com>
2020-04-26 22:21:43 +0900

    postモデルのテスｔ

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,15 +3,39 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  describe 'create'
+  describe '#create' do
+    
+    context 'ログインしている場合' do
+      before do
+        user = create(:user)
+        sign_in user
+      end
+      
+      it 'ファクトリーデータの:postが投稿に成功する確認' do
+        post = create(:post)
+        expect(post).to be_valid
+      end
+      
+      it 'タイトルが文字数255文字を超える時バリデーションエラー' do
+        post = create(:post)
+        post.caption = "a" * 256
+        expect(post).not_to be_valid
+      end
+      
+      it 'タイトルも画像もない場合投稿エラー' do
+        post = Post.new
+        post.caption = nil
+        post.photo_ids = nil
+        post.save
+        expect(post).not_to be_valid
+      end     
+    end
 
-  before do
-    user = create(:user)
-    sign_in user
-  end
-
-  it 'postが成功する確認' do
-    post = create(:post)
-    expect(post).to be_valid
+    context 'ログインしていない場合' do
+      it '投稿に失敗する' do
+        post = create(:post)
+        expect(post).to be_valid
+      end
+    end
   end
 end
