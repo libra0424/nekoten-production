c41a2a2bd53bc1b5cb645d0a6730dbcc0855a8d4
yuzuku <yuzuku04@gmail.com>
2020-04-26 23:15:41 +0900

    インスタンス生成(user・cat)を;etに変更

--- spec/models/cat_spec.rb
+++ spec/models/cat_spec.rb
@@ -4,14 +4,25 @@ require 'rails_helper'
 
 RSpec.describe Cat, type: :model do
   context 'ログインしている場合' do
+    let(:user) {create(:user)}
+    let(:cat) {create(:cat)}
     before do
-      user = create(:user)
       sign_in user
     end
 
     it 'ファクトリーデータの:catが登録に成功する' do
-      post = create(:cat)
-      expect(post).to be_valid
+      expect(cat).to be_valid
     end
+
+    it '名前が文字数50文字を超える時バリデーションエラー' do
+      cat.name = "a" * 51
+      expect(cat).not_to be_valid
+    end
+
+    it '名前がない時バリデーションエラー' do
+      cat.name = nil
+      expect(cat).not_to be_valid
+    end
+
   end
 end
