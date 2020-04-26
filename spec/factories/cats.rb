34fd179fa24bea5a8860716d4ee1a60ff43bdb9d
yuzuku <yuzuku04@gmail.com>
2020-04-26 22:42:48 +0900

    catのファクトリーデータ作成

--- spec/factories/cats.rb
+++ spec/factories/cats.rb
@@ -2,5 +2,12 @@
 
 FactoryBot.define do
   factory :cat do
+    association :user
+    name  {"testcat"}
+    birthday {"2020-01-01"}
+    cat_species {"アメリカンショートヘア"}
+    gender {"1"}
+    coatcolor {"黒"}
+    profile_photo { Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/test.png')) }
   end
 end
