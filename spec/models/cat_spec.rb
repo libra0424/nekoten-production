51362b1a398bc5c11b0de9fe66b3b0c95a69c74b
yuzuku <yuzuku04@gmail.com>
2020-04-27 00:42:35 +0900

    ログアウト状態でのcreate失敗のテスト作成途中

--- spec/models/cat_spec.rb
+++ spec/models/cat_spec.rb
@@ -3,42 +3,49 @@
 require 'rails_helper'
 
 RSpec.describe Cat, type: :model do
-  context 'ログインしている場合' do
-    let(:user) {create(:user)}
+  describe '#create' do
     let(:cat) {create(:cat)}
-    before do
-      sign_in user
+
+    context 'ログインしている場合' do
+      let(:user) {create(:user)}
+      before do
+        sign_in user
+      end
+
+      it 'ファクトリーデータの:catが登録に成功する' do
+        expect(cat).to be_valid
+      end
+
+      it '名前が文字数50文字を超える時バリデーションエラー' do
+        cat.name = "a" * 51
+        expect(cat).not_to be_valid
+      end
+      
+      it '名前がない時バリデーションエラー' do
+        cat.name = nil
+        expect(cat).not_to be_valid
+      end
+
+      it '猫種が文字数50文字を超える時バリデーションエラー' do
+        cat.cat_species = "a" * 51
+        expect(cat).not_to be_valid
+      end
+
+      it '毛色が文字数50文字を超える時バリデーションエラー' do
+        cat.coatcolor = "a" * 51
+        expect(cat).not_to be_valid
+      end
+      
+      it '誕生日が今日以降ならバリデーションエラー' do
+        cat.birthday = Date.today.next_day(1)
+        expect(cat).not_to be_valid
+      end
     end
 
-    it 'ファクトリーデータの:catが登録に成功する' do
-      expect(cat).to be_valid
-    end
+    context 'ログインしていない場合' do
+      it 'ファクトリーデータの:catが登録に失敗する' do
 
-    it '名前が文字数50文字を超える時バリデーションエラー' do
-      cat.name = "a" * 51
-      expect(cat).not_to be_valid
+      end
     end
-    
-    it '名前がない時バリデーションエラー' do
-      cat.name = nil
-      expect(cat).not_to be_valid
-    end
-
-    it '猫種が文字数50文字を超える時バリデーションエラー' do
-      cat.cat_species = "a" * 51
-      expect(cat).not_to be_valid
-    end
-
-    it '毛色が文字数50文字を超える時バリデーションエラー' do
-      cat.coatcolor = "a" * 51
-      expect(cat).not_to be_valid
-    end
-    
-    it '誕生日が今日以降ならバリデーションエラー' do
-      cat.birthday = Date.today.next_day(1)
-      expect(cat).not_to be_valid
-    end
-
-
   end
 end
--- spec/requests/post_spec.rb
+++ spec/requests/post_spec.rb
@@ -3,7 +3,7 @@
 require 'rails_helper'
 
 RSpec.describe PostsController, type: :request do
-
+  let(:post){create(:post)}
   describe '#new' do
     context 'ログインしている場合' do
       let(:user){create(:user)}
@@ -14,6 +14,16 @@ RSpec.describe PostsController, type: :request do
       it '投稿ページへのアクセスに成功する' do
         get "/posts/new"
         expect(response).to be_successful
+      end
+    end
+
+    context 'ログインしていない場合' do
+      it '投稿ページへのアクセスに失敗する' do
+        get "/posts/new"
+        expect(response).to_not be_successful
+      end
+
+      it '投稿に失敗する' do
       end
     end
   end
