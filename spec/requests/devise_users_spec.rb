b484736ffa7802f5fd44588e6fe6ba6afd6ce77f
yuzuku <yuzuku04@gmail.com>
2020-04-27 13:37:17 +0900

    devise テスト完成

--- spec/requests/devise_users_spec.rb
+++ spec/requests/devise_users_spec.rb
@@ -14,7 +14,46 @@ RSpec.describe "UserAuthentications", type: :request do
         post user_registration_path, params: { user: user_params }
         expect(response.status).to eq 302
       end
+      
+      it '認証メールが送信されること' do
+        post user_registration_path, params: { user: user_params }
+        expect(ActionMailer::Base.deliveries.size).to eq 1
+      end
+      
+      it 'createが成功すること' do
+        expect do
+          post user_registration_path, params: { user: user_params}
+        end.to change(User, :count).by 1
+      end
+      
+      # it 'リダイレクトされること' do
+      #   post user_registration_path, params: { user: user_params }
+      #   expect(response).to redirect_to root_url
+      # end
+    end
+
+    context 'パラメーターが不正な場合' do
+      it 'リクエストが成功すること' do
+        post user_registration_path, params: { user: invalid_user_params}
+        expect(response.status).to eq 200
+      end
+
+      it '認証メールが送信されないこと' do
+        post user_registration_path, params: { user: invalid_user_params }
+        expect(ActionMailer::Base.deliveries.size).to eq 0
+      end
+
+      it 'createが失敗すること' do
+        expect do
+          post user_registration_path, params: { user: invalid_user_params}
+        end.to_not change(User, :count)
+      end
+
+
+      it 'エラーが表示されること' do
+        post user_registration_path, params: { user: invalid_user_params }
+        expect(response.body).to include 'Nameが入力されていません。'
+      end
     end
   end
-  
 end
