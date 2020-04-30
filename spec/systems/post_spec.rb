5cc9b77066863df3d957a7fe718580a6af9e5b89
yuzuku <yuzuku04@gmail.com>
2020-05-01 01:03:40 +0900

    system/postテストの投稿テストを追加

---
+++ spec/systems/post_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.describe PostsController, type: :system do
+  let(:user){create(:user)}
+  let(:other_user){create(:user2)}
+  let(:build_post){build(:post)}
+  let(:create_other_user_post){create(:post, user:other_user)}
+  let(:post_params){ attributes_for(:post)}
+  let(:invalid_post_params){ attributes_for(:post, caption:"", photos: nil)} #タイトルも写真も空白
+
+
+  describe '#new' do
+    context 'ログインしている場合' do
+      before do
+        sign_in user
+      end
+      
+      it '投稿ページへのアクセスに成功する' do
+        visit new_post_path
+        expect(page).to have_content '投稿画面'
+      end
+
+      context 'パラメータが妥当な場合' do
+        it '投稿できる' do
+          expect {
+            visit new_post_path       
+            fill_in 'post[caption]', with: build_post.caption
+            attach_file 'post_photos_attributes_0_image', 'spec/test.png'           
+            click_button '投稿する'
+          }.to change(Post, :count).by(1)
+        end
+      end
+    end
+  end
+end
