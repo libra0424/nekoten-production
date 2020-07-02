6afeea090b96bd2927805ccd460601ab0214ba43
yuzuku <yuzuku04@gmail.com>
2020-07-02 20:34:27 +0900

    ass pagenation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,7 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_in_path_for(resource) 
-    index_new_path
+    index_new_posts_path
   end
 
   def after_sign_out_path_for(resource)
